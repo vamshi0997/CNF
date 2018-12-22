c1695ac7305a31c180d91d7a5bc9dde16ef0ccdc
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-12-22 12:23:33 +0530

    nsev

--- CNF_Week_2/server.py
+++ CNF_Week_2/server.py
@@ -29,39 +29,26 @@ def Main():
 				if key == roll:
 					temp = 0
 					personid[c] = key
-					print(key + ' -- Started PUZZLE')
+					print(key + ' Started PUZZLE')
 					Thread(target = att, args = (attlist,c,personid,person)).start()
 			if temp:
 				c.send('ROLL NUMBER-NOT FOUND'.encode())
-				check(person)
 	s.close()
-			
-def check(person):
-	if (active_count() == 2):
-		print('WAIT TIME - 15 Seconds')
-		time.sleep(15)
-		if (active_count() == 2):
-			print('Rollnumbers of Presented Students are: ')
-			print(person)
 
 def att(attlist, c, personid,person):
-	lis = attlist[personid[c]]
+	nlist = attlist[personid[c]]
 	while True:
 
-		c.send(('SECRETQUESTION-' + lis[0]).encode())
+		c.send(('SECRETQUESTION-' + nlist[0]).encode())
 		user_ans = c.recv(1024).decode().split('-')
 
 		if (user_ans[0] == 'SECRETANSWER'):
-			if (user_ans[1] == lis[1]):
+			if (user_ans[1] == nlist[1]):
 				person.append(personid[c])
-				print(str(personid[c]) + ' -- Present')
+				print(str(personid[c]) + '  Present')
 				c.send('ATTENDANCE SUCCESS'.encode())
-				check(person)
 			else:
 				c.send('ATTENDANCE FAILURE'.encode())
-		else:
-			c.send('Invalid Syntax'.encode())
-
 
 if __name__ == '__main__':
 	Main()
